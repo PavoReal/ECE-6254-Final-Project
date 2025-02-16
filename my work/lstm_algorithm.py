8c5001dbf4f86e0bbcd97faeff6156397bb95019
Sandeep Anushka <SandeepAnushka@JohnDeere.com>
2025-02-16 17:12:00 -0600

    printing dataset

--- my work/lstm_algorithm.py
+++ my work/lstm_algorithm.py
@@ -6,5 +6,21 @@ import tensorflow as tf
 
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import LSTM
+from tensorflow.python.keras.layers import LSTMV1
+from datetime import date
+import yfinance as yf
 
+START = "2010-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+# Define a function to load the dataset
+
+def load_data(ticker):
+    data = yf.download(ticker, start=START, end=TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data = load_data("VOO")
+print(data.head())
+
+data.to_csv(r"C:/Users/rs8c8bh/SP500StockPrc.csv")
