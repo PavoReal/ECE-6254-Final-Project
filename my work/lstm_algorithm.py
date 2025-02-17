dd4407ca112c241322c61bb7a5253ac01deac075
Sandeep Anushka <SandeepAnushka@JohnDeere.com>
2025-02-16 19:28:20 -0600

    didn't compile yet, to be continued

--- my work/lstm_algorithm.py
+++ my work/lstm_algorithm.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, Dropout
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import LSTMV1
+from tensorflow.python.keras.layers import LSTM
 from datetime import date
 import yfinance as yf
 
@@ -24,3 +24,41 @@ data = load_data("VOO")
 print(data.head())
 
 data.to_csv(r"C:/Users/rs8c8bh/SP500StockPrc.csv")
+df = data
+
+mid_last_value = df.iloc[-1, 0]/2
+train = df.iloc[:mid_last_value]
+test = df.iloc[mid_last_value:]
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(train)
+scaled_train = scaler.transform(train)
+scaled_test = scaler.transform(test)
+
+print(scaled_train)
+
+model = Sequential()
+model.add(LSTM(units = 50, activation = 'relu', return_sequences=True
+              ,input_shape = (x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+
+model.add(LSTM(units = 60, activation = 'relu', return_sequences=True))
+model.add(Dropout(0.3))
+
+
+model.add(LSTM(units = 80, activation = 'relu', return_sequences=True))
+model.add(Dropout(0.4))
+
+
+model.add(LSTM(units = 120, activation = 'relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(units = 1))
+
+# plotting data
+plt.title("Close Price Visualization")
+plt.plot(df.Close)
+plt.show()
+
