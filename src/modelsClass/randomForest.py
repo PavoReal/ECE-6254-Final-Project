38792964f74fb1e501d3ed1693ed969ef542f027
Sandeep Anushka <SandeepAnushka@JohnDeere.com>
2025-03-29 19:15:53 -0500

    working model!- MSE: 0.0422222, MAE: 0.144285
    MSE- mean squared error, MAE- mean absolute error

--- src/modelsClass/randomForest.py
+++ src/modelsClass/randomForest.py
@@ -1,12 +1,11 @@
 import numpy as np
-import seaborn as sns
 import yfinance as yf
 import pandas as pd
 import os
 import time
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # RANDOM FOREST
 
@@ -19,8 +18,7 @@ from sklearn.metrics import mean_squared_error
 # Load model- yfinance
 def getDataSet(ticker, startDate, endDate, loadFromOnline):
     # load model from yfinance or anything from keras
-    # Download the data
-
+    # Do
     if loadFromOnline:
         try:
             data = yf.download(ticker, start=startDate, end=endDate)
@@ -30,7 +28,6 @@ def getDataSet(ticker, startDate, endDate, loadFromOnline):
             time.sleep(60) #wait 60 seconds.
             try:
                 data = yf.download(ticker, start=start_date, end=end_date)
-                print(data.head())
             except Exception as inner_e:
                 print(f"Second attempt failed: {inner_e}")
                 return None
@@ -43,21 +40,25 @@ def getDataSet(ticker, startDate, endDate, loadFromOnline):
             data = pd.read_csv(filepath)
             return data
 
-def create_lag(seq_length):
-
+def create_lag(seq_length, dataset, feature_name):
     # return new X and y from lag data
-    return
-#TO-DO
-# seq_length would be the number of prior data points used to predict the new data point
-def create_randomforest_model(seq_length):
-    # have the x values
-    # set the y values
+    X = np.zeros((len(dataset) - seq_length, seq_length))
+    for i in range(seq_length, len(dataset)):
+        X[i - seq_length] = dataset[feature_name][i - seq_length : i].values
+    y = dataset[feature_name][seq_length : ].values
+    return X, y
 
+#TO-DO
+# add GridSearch for parameter tuning
+# GridSearchCV(estimator=RandomForestRegressor(random_state=42), param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+
+
+# seq_length would be the number of prior data points used to predict the new data point
+def create_randomforest_model():
     # creating RF regressor
     rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42, criterion='squared_error', max_depth=None, min_samples_split=2
                                          , min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='sqrt', max_leaf_nodes=None, min_impurity_decrease=0,
                                          bootstrap=True, oob_score=False,n_jobs=None,verbose=0,warm_start=False,ccp_alpha=0.0,max_samples=None)
-    
     return rf_regressor
 
 def split_dataset(dataset, train_ratio, ticker):
@@ -73,16 +74,42 @@ def split_dataset(dataset, train_ratio, ticker):
         test.to_csv(test_file, index=False)
     return train, test
 
+def train_model(xtrain, ytrain, xtest):
+    theRFmodel = create_randomforest_model()
+    theRFmodel.fit(xtrain, ytrain)
+
+    # predicting the test set
+    yPredict = theRFmodel.predict(xtest)
+    return yPredict
+
+def model_evaluation(yPred, yCorrect):
+    meanSqErr = mean_squared_error(yCorrect, yPred)
+    meanAbsErr = mean_absolute_error(yCorrect, yPred)
+    return meanSqErr, meanAbsErr
 
 #----------------------------------------------------------------------------------------------------------------
 # Define the ticker symbol
 ticker = "CEF"
-
+myFeature = "Close"
 # Define the date range
 start_date = "2020-01-01"
 end_date = "2023-12-31"
 myDataset = getDataSet(ticker,start_date,end_date,False)
 train_set, test_set = split_dataset(myDataset, 0.8, ticker)
 
-#save the data to a csv file.
-#myDataset.to_csv("MyData_historical_data.csv")
+# TRAINING
+lag5 = 5
+X_lag5_train, ylag5_train = create_lag(lag5, train_set, myFeature)
+X_lag5_test, y_lag5_test = create_lag(lag5, test_set, myFeature)
+
+# saving training data to CSV
+output_file_pd = 'my_array_pd.csv'
+df = pd.DataFrame(X_lag5_train) #Create a pandas dataframe from the numpy array.
+df['y_lag5Train'] = ylag5_train
+df.to_csv(output_file_pd, index=False)
+
+# training model!
+yPred_lag5 = train_model(X_lag5_train, ylag5_train, X_lag5_test)
+mean_sq_err, mean_abs_err = model_evaluation(yPred_lag5, y_lag5_test)
+
+print("MSE!: ", mean_sq_err, "AbsErr!: ", mean_abs_err)
